4432c3049253fa1b0eb33dd1a98f2ce6d1ede6f5
Abdullah <abdullah.3prep@gmail.com>
2026-05-05 11:53:16 +0300

    Reset: Delete all files for fresh start v1

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('v5.1-vip').then(c => c.addAll(['./', './index.html', './style.css', './script.js'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
